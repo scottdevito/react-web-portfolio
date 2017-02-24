d6154c4a2ce16eca11b901284d1c8a21eb953b20
psilotec <scottadevito@gmail.com>
2017-02-23 20:39:05 -0500

    Fix lint warnings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './styles/App.css';
 
 import { Banner } from './components/banner';
--- src/components/banner.js
+++ src/components/banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Banner = () => {
     return (
         <div className="banner-container">
-            <img className="banner" src="https://s3.amazonaws.com/sdevito-web-portfolio/port-banner.png" />
+            <img className="banner" src="https://s3.amazonaws.com/sdevito-web-portfolio/port-banner.png" role="presentation" />
         </div>
     );
 }
