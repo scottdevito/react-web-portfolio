88c4989352c96406d33c25450b6824a7816e47bf
psilotec <scottadevito@gmail.com>
2017-03-24 15:31:11 -0400

    Change banner behavior for mobile

--- src/components/banner.js
+++ src/components/banner.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 export const Banner = () => {
     return (
-        <div className="banner-container">
-            <img className="banner" src="https://s3.amazonaws.com/sdevito-web-portfolio/port-banner.png" role="presentation" />
-        </div>
+        <div className="banner"></div>
     );
 }
