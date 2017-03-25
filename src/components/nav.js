83d0f2c22c5ca2fb2cbdc53c746f4bbb7068bdeb
psilotec <scottadevito@gmail.com>
2017-03-24 21:34:22 -0400

    FIx  bootstrap and react-router issue

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { IndexLink, Link } from 'react-router';
 import { Navbar, NavItem, Nav } from 'react-bootstrap';
+import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
 
 export const Navigation = () => {
     return (
         <Navbar fixedTop collapseOnSelect>
             <Navbar.Header>
             <Navbar.Brand>
-                <IndexLink to="/" activeClassName="active">Scott DeVito</IndexLink>
+                <a href="/">Scott DeVito</a>
             </Navbar.Brand>
             <Navbar.Toggle />
             </Navbar.Header>
             <Navbar.Collapse>
             <Nav>
-                <NavItem eventKey={1}><IndexLink to="/" activeClassName="active">Home</IndexLink></NavItem>
-                <NavItem eventKey={2}><Link to="/resume" activeClassName="active">Resume</Link></NavItem>
-                <NavItem eventKey={3}><Link to="/contact" activeClassName="active">Contact</Link></NavItem>
+                <IndexLinkContainer to="/">
+                    <NavItem>Home</NavItem>
+                </IndexLinkContainer>
+                <LinkContainer to="/resume">
+                    <NavItem>Resume</NavItem>
+                </LinkContainer>                
+                <LinkContainer to="/contact">
+                    <NavItem>Contact</NavItem>
+                </LinkContainer>
             </Nav>
             </Navbar.Collapse>
         </Navbar>
