0a9e791029651ac0b0772c4c9da3d4538bf60542
psilotec <scottadevito@gmail.com>
2017-03-26 10:38:44 -0400

    Adjust Contact component styling

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,11 +4,13 @@ export const Contact = ( ) => {
     return(
         <div className="contact">
             <div className="contact-container">
-                <a href="mailto:scottadevito@gmail.com">
-                    <div className="contact-btn"><h5>Email</h5></div>
+                <a href="https://github.com/psilotec" target="_blank">                
+                    <div className="contact-btn"><h5>Github</h5></div>
                 </a>
-                
-                <img className="email-logo" src="https://s3.amazonaws.com/sdevito-web-portfolio/mailIcon1.png" role="presentation" />
+
+                <a href="mailto:scottadevito@gmail.com">
+                    <img className="email-logo" src="https://s3.amazonaws.com/sdevito-web-portfolio/mailIcon1.png" role="presentation" />
+                </a>
 
                 <a href="https://www.linkedin.com/pub/scott-devito/91/239/34" target="_blank">
                     <div className="contact-btn"><h5>LinkedIn</h5></div>
