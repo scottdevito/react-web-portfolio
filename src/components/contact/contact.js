672b089804b3c9614df8a75883f9be5197427278
Scott DeVito <scottadevito@gmail.com>
2017-08-23 20:02:25 -0400

    Github url and resume changes

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
-export const Contact = ( ) => {
-    return(
-        <div className="contact">
-            <div className="contact-container">
-                <a href="https://github.com/psilotec" target="_blank">                
-                    <div className="contact-btn"><h5>Github</h5></div>
-                </a>
+export const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="contact-container">
+        <a href="https://github.com/scottdevito" target="_blank">
+          <div className="contact-btn">
+            <h5>Github</h5>
+          </div>
+        </a>
 
-                <a href="mailto:scottadevito@gmail.com">
-                    <img className="email-logo" src="https://s3.amazonaws.com/sdevito-web-portfolio/mailIcon1.png" role="presentation" />
-                </a>
+        <a href="mailto:scottadevito@gmail.com">
+          <img
+            className="email-logo"
+            src="https://s3.amazonaws.com/sdevito-web-portfolio/mailIcon1.png"
+            role="presentation"
+          />
+        </a>
 
-                <a href="https://www.linkedin.com/pub/scott-devito/91/239/34" target="_blank">
-                    <div className="contact-btn"><h5>LinkedIn</h5></div>
-                </a>
-            </div>
-        </div>
-    );
+        <a href="https://www.linkedin.com/scott-devito" target="_blank">
+          <div className="contact-btn">
+            <h5>LinkedIn</h5>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 };
--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -6,51 +6,85 @@ import { Experience } from './experience';
 import { Education } from './education';
 
 class Resume extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        //State to keep track of which child component(s) to display
-        this.state = {
-            visibleComponent: "full"
-        };
-    }
+    //State to keep track of which child component(s) to display
+    this.state = {
+      visibleComponent: 'full',
+    };
+  }
 
-    //Event Handler for resume filter
-    toggleResumeFilter(id, event) {
-        this.setState({
-            visibleComponent: id
-        });
-    }
-    render() {
-        return(
-            <div className="resume">
-                <div className="filters">
-                    <div className="btn-group btn-group-justified" role="group" aria-label="...">
-                        <div className="btn-group" role="group">
-                            <button type="button" className="btn btn-default" onClick={this.toggleResumeFilter.bind(this, "full")}>Full Resume</button>
-                        </div>
-                        <div className="btn-group" role="group">
-                            <button type="button" className="btn btn-default" onClick={this.toggleResumeFilter.bind(this, "skills")}>Skills</button>
-                        </div>
-                        <div className="btn-group" role="group">
-                            <button type="button" className="btn btn-default" onClick={this.toggleResumeFilter.bind(this, "experience")}>Experience</button>
-                        </div>
-                        <div className="btn-group" role="group">
-                            <button type="button" className="btn btn-default" onClick={this.toggleResumeFilter.bind(this, "education")}>Education</button>
-                        </div>
-                    </div>
-                </div>
-
-                <a className="btn btn-primary btn-sm" href="https://s3.amazonaws.com/sdevito-web-portfolio/ScottDeVitoResume.pdf" target="_blank" role="button">Download Resume</a>
-
-                <Full visible={this.state.visibleComponent === "full"} />
-                <Skills visible={this.state.visibleComponent === "skills"} />
-                <Experience visible={this.state.visibleComponent === "experience"} />
-                <Education visible={(this.state.visibleComponent === "education")} />
-
+  //Event Handler for resume filter
+  toggleResumeFilter(id, event) {
+    this.setState({
+      visibleComponent: id,
+    });
+  }
+  render() {
+    return (
+      <div className="resume">
+        <div className="filters">
+          <div
+            className="btn-group btn-group-justified"
+            role="group"
+            aria-label="..."
+          >
+            <div className="btn-group" role="group">
+              <button
+                type="button"
+                className="btn btn-default"
+                onClick={this.toggleResumeFilter.bind(this, 'full')}
+              >
+                Full Resume
+              </button>
             </div>
-        );
-    }
-};
+            <div className="btn-group" role="group">
+              <button
+                type="button"
+                className="btn btn-default"
+                onClick={this.toggleResumeFilter.bind(this, 'skills')}
+              >
+                Skills
+              </button>
+            </div>
+            <div className="btn-group" role="group">
+              <button
+                type="button"
+                className="btn btn-default"
+                onClick={this.toggleResumeFilter.bind(this, 'experience')}
+              >
+                Experience
+              </button>
+            </div>
+            <div className="btn-group" role="group">
+              <button
+                type="button"
+                className="btn btn-default"
+                onClick={this.toggleResumeFilter.bind(this, 'education')}
+              >
+                Education
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <a
+          className="btn btn-primary btn-sm"
+          href="https://s3.amazonaws.com/sdevito-web-portfolio/Scott_DeVito_SoftwareEngineer_Resume.pdf"
+          target="_blank"
+          role="button"
+        >
+          Download Resume
+        </a>
+
+        <Full visible={this.state.visibleComponent === 'full'} />
+        <Skills visible={this.state.visibleComponent === 'skills'} />
+        <Experience visible={this.state.visibleComponent === 'experience'} />
+        <Education visible={this.state.visibleComponent === 'education'} />
+      </div>
+    );
+  }
+}
 
 export default Resume;
