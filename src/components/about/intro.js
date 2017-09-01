e4e9915ffdea4d8577f08aa96dd8d3185e9de76c
Scott DeVito <scottadevito@gmail.com>
2017-09-01 00:51:00 -0400

    Add clouds and cloud animation to Intro

--- src/components/about/intro.js
+++ src/components/about/intro.js
@@ -20,6 +20,27 @@ export const Intro = () => {
         </div>
       </div>
       <div className="footer">
+        <div className="cloud-animation-div">
+          <img
+            className="clouds cloud-first"
+            src="https://s3.amazonaws.com/sdevito-web-portfolio/cloud3.svg"
+            alt="clouds"
+          />
+        </div>
+        <div className="cloud-animation-div">
+          <img
+            className="clouds cloud-second"
+            src="https://s3.amazonaws.com/sdevito-web-portfolio/cloud3.svg"
+            alt="clouds"
+          />
+        </div>
+        <div className="cloud-animation-div">
+          <img
+            className="clouds cloud-third"
+            src="https://s3.amazonaws.com/sdevito-web-portfolio/cloud3.svg"
+            alt="clouds"
+          />
+        </div>
         <img
           className="mountains"
           src="https://s3.amazonaws.com/sdevito-web-portfolio/mountains-tent.svg"
