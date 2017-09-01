21af12a564c97c84f92addc1c0061b4fdfa6a9d0
Scott DeVito <scottadevito@gmail.com>
2017-09-01 00:53:19 -0400

    Refactor footer into its own component

---
+++ src/components/about/footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer">
+    <div className="cloud-animation-div">
+      <img
+        className="clouds cloud-first"
+        src="https://s3.amazonaws.com/sdevito-web-portfolio/cloud3.svg"
+        alt="clouds"
+      />
+    </div>
+    <div className="cloud-animation-div">
+      <img
+        className="clouds cloud-second"
+        src="https://s3.amazonaws.com/sdevito-web-portfolio/cloud3.svg"
+        alt="clouds"
+      />
+    </div>
+    <div className="cloud-animation-div">
+      <img
+        className="clouds cloud-third"
+        src="https://s3.amazonaws.com/sdevito-web-portfolio/cloud3.svg"
+        alt="clouds"
+      />
+    </div>
+    <img
+      className="mountains"
+      src="https://s3.amazonaws.com/sdevito-web-portfolio/mountains-tent.svg"
+      alt="mountain img"
+    />
+  </div>
+);
+
+export default Footer;
