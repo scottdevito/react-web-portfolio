331043dcbd3f88f30e9b3ed8fb8cb0bb7127ae5b
Scott DeVito <scottadevito@gmail.com>
2017-11-02 23:32:35 -0400

    Add projects and project images

--- src/components/projects/project_card.js
+++ src/components/projects/project_card.js
@@ -3,16 +3,9 @@ import React from 'react';
 const ProjectCard = props => (
   <div className="project-div">
     <div className="project-card">
-      <h4 className="card-text">{props.name}</h4>
-      <img
-        className="project-img"
-        src="https://avatars3.githubusercontent.com/u/16454542?v=4&s=460"
-        alt="github profile"
-      />
-      <p className="card-text">
-        This is a description of the project. Not too long, not too short, just
-        right.
-      </p>
+      <h3 className="card-title">{props.name}</h3>
+      <img className="project-img" src={props.img} alt="project" />
+      <p className="card-text">{props.desc}</p>
     </div>
   </div>
 );
--- src/components/projects/projects_display.js
+++ src/components/projects/projects_display.js
@@ -13,10 +13,26 @@ const ProjectsDisplay = () => (
     </div>
 
     <div className="project-display">
-      <ProjectCard name="Lawman Jiu Jitsu Belt List" />
-      <ProjectCard name="React Web Portfolio" />
-      <ProjectCard name="Weather Cheese Pairing" />
-      <ProjectCard name="Lawman Jiu Jitsu Teaching Assistant" />
+      <ProjectCard
+        name="Lawman Jiu Jitsu Belt List"
+        img="https://s3.amazonaws.com/sdevito-web-portfolio/LMJJ+Monitor.svg"
+        desc="A React site to allow LMJJ students to log in and access technique information for their current rank."
+      />
+      <ProjectCard
+        name="Good Days"
+        img="https://s3.amazonaws.com/sdevito-web-portfolio/Good+Days+Phone.svg"
+        desc="A React Native app to help you reflect on what your perfect day would look like."
+      />
+      <ProjectCard
+        name="React Web Portfolio"
+        img="https://s3.amazonaws.com/sdevito-web-portfolio/RWP+Monitor.svg"
+        desc="A React site to showcase my projects and share my contact information."
+      />
+      <ProjectCard
+        name="Lawman Jiu Jitsu Teaching Assistant"
+        img="https://s3.amazonaws.com/sdevito-web-portfolio/LMJJTA+Monitor.svg"
+        desc="An interactive teaching assistant app for LMJJ instructors to use on the mat."
+      />
     </div>
 
     <div className="header-box">
@@ -26,6 +42,20 @@ const ProjectsDisplay = () => (
         alt="github profile"
       />
       <h2 className="header-text">Long Island Javascript Meetup</h2>
+    </div>
+
+    <div className="project-display">
+      <ProjectCard name="Array Methods Presentation" />
+      <ProjectCard />
+    </div>
+
+    <div className="header-box">
+      <img
+        className="header-img"
+        src="https://avatars3.githubusercontent.com/u/31260897?v=4&s=200"
+        alt="figma profile"
+      />
+      <h2 className="header-text">Designs</h2>
     </div>
 
     <div className="project-display">
