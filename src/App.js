d7fcc43f98dad0623b068ed19b09dd74395c7b71
Scott DeVito <scottadevito@gmail.com>
2018-02-17 09:15:16 -0500

    Update react-router and move routing to App

--- src/App.js
+++ src/App.js
@@ -1,51 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { Banner } from './components/banner';
 import { Navigation } from './components/navigation';
+import { NotFound } from './components/not_found';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import { Contact } from './components/contact/contact';
+import ProjectDisplay from './components/projects/projects_display';
 
-export const App = ({ children }) => {
+export const App = () => {
   return (
-    <div>
-      <Navigation />
-      <Banner />
-      {children}
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        <Banner />
+        <Route exact path="/" component={About} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/projects" component={ProjectDisplay} />
+        <Route path="/contact" component={Contact} />
+
+        <Route path="*" component={NotFound} />
+      </div>
+    </Router>
   );
-};
-
-// JQuery for Nav and Banner blur onscroll effect
-// About component onscroll opacity effect
-// TODO: refactor with React animation
-window.onscroll = function(e) {
-  var banner = document.getElementsByClassName('banner')[0],
-    nav = document.getElementsByClassName('navbar')[0],
-    projs = document.getElementsByClassName('projects-display')[0],
-    resume = document.getElementsByClassName('resume')[0],
-    contact = document.getElementsByClassName('contact')[0],
-    intro = document.getElementsByClassName('intro-container')[0],
-    range = 50,
-    scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-  // Intro is undefined if the About component isn't active
-  if (scrollTop > range && intro !== undefined) {
-    intro.classList.add('intro-container-scroll');
-    banner.classList.add('blurred');
-    nav.classList.add('nav-scroll');
-  } else if (scrollTop < range && intro !== undefined) {
-    intro.classList.remove('intro-container-scroll');
-    banner.classList.remove('blurred');
-    nav.classList.remove('nav-scroll');
-  } else if (
-    scrollTop > range &&
-    (contact !== undefined || resume !== undefined || projs !== undefined)
-  ) {
-    banner.classList.add('blurred');
-    nav.classList.add('nav-scroll');
-  } else if (
-    scrollTop < range &&
-    (contact !== undefined || resume !== undefined || projs !== undefined)
-  ) {
-    banner.classList.remove('blurred');
-    nav.classList.remove('nav-scroll');
-  }
 };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import './styles/index.scss';
 import 'typeface-roboto';
 
-import { NotFound } from './components/not_found';
-import About from './components/about/about';
-import Resume from './components/resume/resume';
-import { Contact } from './components/contact/contact';
-import ProjectDisplay from './components/projects/projects_display';
-
-render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={About} />
-      <Route path="/resume" component={Resume} />
-      <Route path="/projects" component={ProjectDisplay} />
-      <Route path="/contact" component={Contact} />
-    </Route>
-    <Route path="*" component={NotFound} />
-  </Router>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
