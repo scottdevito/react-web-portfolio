94f0fb0d60589d3f34e8f487eb3a1f38b74def03
Scott DeVito <scottadevito@gmail.com>
2018-02-17 12:58:45 -0500

    Refactor App/Navigation with styled-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import styled from 'styled-components';
 import { Banner } from './components/banner';
 import { Navigation } from './components/navigation';
 import { NotFound } from './components/not_found';
@@ -12,9 +12,16 @@ import ProjectDisplay from './components/projects/projects_display';
 export const App = () => {
   return (
     <Router>
-      <div>
+      <StyledApp>
         <Navigation />
         <Banner />
+        <Route
+          pattern="/"
+          render={props => {
+            window.scrollTo(0, 0);
+            return null;
+          }}
+        />
         <Switch>
           <Route exact path="/" component={About} />
           <Route path="/resume" component={Resume} />
@@ -22,7 +29,14 @@ export const App = () => {
           <Route path="/contact" component={Contact} />
           <Route component={NotFound} />
         </Switch>
-      </div>
+      </StyledApp>
     </Router>
   );
 };
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+`;
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,32 +1,56 @@
 import React from 'react';
-import { Navbar, NavItem, Nav } from 'react-bootstrap';
-import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import AppBar from 'material-ui/AppBar';
+import Button from 'material-ui/Button';
 
 export const Navigation = () => {
   return (
-    <Navbar fixedTop collapseOnSelect fluid>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="/">Scott DeVito</a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-          <IndexLinkContainer to="/">
-            <NavItem>Home</NavItem>
-          </IndexLinkContainer>
-          <LinkContainer to="/projects">
-            <NavItem>Projects</NavItem>
-          </LinkContainer>
-          <LinkContainer to="/resume">
-            <NavItem>Resume</NavItem>
-          </LinkContainer>
-          <LinkContainer to="/contact">
-            <NavItem>Contact</NavItem>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <StyledAppBar position="sticky" color="default">
+      <StyledNavLinks>
+        <StyledNavLink>
+          <NavLink to="/" style={{ textDecoration: 'none' }}>
+            <Button>Home</Button>
+          </NavLink>
+        </StyledNavLink>
+        <StyledNavLink>
+          <NavLink to="/projects" style={{ textDecoration: 'none' }}>
+            <Button>Projects</Button>
+          </NavLink>
+        </StyledNavLink>
+        <StyledNavLink>
+          <NavLink to="/resume" style={{ textDecoration: 'none' }}>
+            <Button>Resume</Button>
+          </NavLink>
+        </StyledNavLink>
+        <StyledNavLink>
+          <NavLink to="/contact" style={{ textDecoration: 'none' }}>
+            <Button>Contact</Button>
+          </NavLink>
+        </StyledNavLink>
+      </StyledNavLinks>
+    </StyledAppBar>
   );
 };
+
+const StyledAppBar = styled(AppBar)`
+  width: 100%;
+  height: 3.5em;
+`;
+
+const StyledNavLinks = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-content: center;
+  height: 100%;
+`;
+
+const StyledNavLink = styled.p`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  height: 100%;
+  margin: 0;
+`;
